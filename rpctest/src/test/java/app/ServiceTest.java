3650810197060c44c4c81f04c3228f86238fecf9
chengys4 <chengys4@ziroom.com>
2018-03-02 16:33:30 +0800

    0.0.1

--- rpctest/src/test/java/app/ServiceTest.java
+++ rpctest/src/test/java/app/ServiceTest.java
@@ -1,109 +1,24 @@
 package app;
 
-
 import com.ziroom.bsrd.client.IHelloService;
-import com.ziroom.bsrd.client.IPersonService;
-import com.ziroom.bsrd.client.Person;
-import com.ziroom.bsrd.rpc.IAsyncObjectProxy;
 import com.ziroom.bsrd.rpc.RpcClient;
-import com.ziroom.bsrd.rpc.netty.RPCFuture;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.ziroom.bsrd.rpc.zk.ServiceDiscovery;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:client-spring.xml")
+/**
+ * @author chengys4
+ *         2018-03-01 10:11
+ **/
 public class ServiceTest {
-
-    @Autowired
-    private RpcClient rpcClient;
-
-    @Before
-    public void init() throws Exception {
-        Thread.sleep(3 * 1000L);
-    }
-
-
-    @Test
-    public void helloTest1() {
+    public static void main(String[] args) throws Exception {
+        ServiceDiscovery serviceDiscovery = new ServiceDiscovery();
+        serviceDiscovery.setRegistryAddress("10.16.37.112:3181");
+        serviceDiscovery.setNamespace("rpc");
+        RpcClient rpcClient = new RpcClient(serviceDiscovery);
+        rpcClient.init();
+        Thread.sleep(3000L);
         IHelloService helloService = rpcClient.create(IHelloService.class);
         String result = helloService.hello("World");
-        Assert.assertEquals("Hello! World", result);
+        System.out.println(result);
+        rpcClient.stop();
     }
-
-    @Test
-    public void helloTest2() {
-        IHelloService helloService = rpcClient.create(IHelloService.class);
-        Person person = new Person("Yong", "Huang");
-        String result = helloService.hello(person);
-        Assert.assertEquals("Hello! Yong Huang", result);
-    }
-
-    @Test
-    public void helloPersonTest() {
-        IPersonService personService = rpcClient.create(IPersonService.class);
-        int num = 5;
-        List<Person> persons = personService.GetTestPerson("xiaoming", num);
-        List<Person> expectedPersons = new ArrayList<>();
-        for (int i = 0; i < num; i++) {
-            expectedPersons.add(new Person(Integer.toString(i), "xiaoming"));
-        }
-        assertThat(persons, equalTo(expectedPersons));
-
-        for (int i = 0; i < persons.size(); ++i) {
-            System.out.println(persons.get(i));
-        }
-    }
-
-    @Test
-    public void helloFutureTest1() throws ExecutionException, InterruptedException {
-        IAsyncObjectProxy helloService = rpcClient.createAsync(IHelloService.class);
-        RPCFuture result = helloService.call("hello", "World");
-        Assert.assertEquals("Hello! World", result.get());
-    }
-
-    @Test
-    public void helloFutureTest2() throws ExecutionException, InterruptedException {
-        IAsyncObjectProxy helloService = rpcClient.createAsync(IHelloService.class);
-        Person person = new Person("Yong", "Huang");
-        RPCFuture result = helloService.call("hello", person);
-        Assert.assertEquals("Hello! Yong Huang", result.get());
-    }
-
-    @Test
-    public void helloPersonFutureTest1() throws ExecutionException, InterruptedException {
-        IAsyncObjectProxy helloPersonService = rpcClient.createAsync(IPersonService.class);
-        int num = 5;
-        RPCFuture result = helloPersonService.call("GetTestPerson", "xiaoming", num);
-        List<Person> persons = (List<Person>) result.get();
-        List<Person> expectedPersons = new ArrayList<>();
-        for (int i = 0; i < num; i++) {
-            expectedPersons.add(new Person(Integer.toString(i), "xiaoming"));
-        }
-        assertThat(persons, equalTo(expectedPersons));
-
-        for (int i = 0; i < num; ++i) {
-            System.out.println(persons.get(i));
-        }
-    }
-
-    @After
-    public void setTear() {
-        if (rpcClient != null) {
-            rpcClient.stop();
-        }
-    }
-
 }
